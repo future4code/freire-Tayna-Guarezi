115a9067928bb583485da626550c775f16abb6fb
TaynaGuarezi <tayna.guarezi@gmail.com>
2022-05-18 22:37:21 -0300

    Exercicios de laços

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,20 @@
+//exercicios interpretação de código
+//1-a) valor = valor mais o indice, no console aparece 10
+//2-a)os numeros menores que 18, 10,11,12,15 b) não entendi a pergunta
+//3 
+//*
+//**
+//*** 
+//**** 
+
+//exercicios de escrita de código
+//1
+
+console.log('ola')
+
+
+
+
+
+
+
